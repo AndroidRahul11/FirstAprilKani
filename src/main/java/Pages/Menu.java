80850735f86e531ffc221f7244f2cb14b7042094
rahulvyavahare <rahul.vyavahare@clover.com>
2023-04-04 16:40:32 +0530

    2Tcs

--- src/main/java/Pages/Menu.java
+++ src/main/java/Pages/Menu.java
@@ -12,6 +12,14 @@ public class Menu extends BaseClass {
     @AndroidFindBy (xpath ="//android.view.ViewGroup/android.widget.TextView[@text=\"\uE5D2\"]") private WebElement menubtn;
     @AndroidFindBy (xpath = "//android.widget.TextView[@text=\"Sales Order\"]") private WebElement SalesOrder;
     @AndroidFindBy (xpath ="//android.view.ViewGroup/android.widget.TextView[@text=\"\uE145\"]") private WebElement plusbtn;
+    @AndroidFindBy (xpath ="//android.view.ViewGroup/android.widget.ImageView") private WebElement selCustomer;
+    @AndroidFindBy (xpath ="//android.widget.TextView[@text=\"1 vish test\"]") private WebElement Customerselected;
+
+    @AndroidFindBy (xpath ="//android.widget.TextView[@text=\"Customer Details\"]") private WebElement customerDetails;
+
+
+
+
 
     AndroidDriver driver;
     public Menu(AndroidDriver driver) {
@@ -27,6 +35,12 @@ public class Menu extends BaseClass {
         util.click(SalesOrder);
         util.waitforVisibility(plusbtn, 10);
         util.click(plusbtn);
+        util.waitforVisibility(selCustomer, 10);
+        util.click(selCustomer);
+        util.waitforVisibility(Customerselected, 10);
+        util.click(Customerselected);
+        util.waitforVisibility(customerDetails, 10);
+        System.out.println("Navigated to Customer details page");
     }
 
 }
